278bb3e25f8a3a43687db3bfae55800bc5d60ab4
Alexander Borovsky <kycok.6o6pa@gmail.com>
2016-03-31 01:05:19 +0300

    Improvements

---
+++ test/connectionFSM.js
@@ -0,0 +1,7 @@
+/**
+ * Created by boba on 31.03.16.
+ */
+var connectionFSM = require('../lib/connectionFSM.js');
+var config = require('./config.js');
+var should = require('should');
+var connection = new connectionFSM(config);
